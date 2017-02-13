07494a8aeaeecc0660e0f7b238c63ed64cf67b40
Bryan <btteh@hotmail.com>
2017-02-13 09:16:43 +0800

    Adding in placeholder files so that the asset folders don't automatically delete themselves on unity start-up.

---
+++ SP4/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour 
+{
+    public Button Play, Options;
+	// Use this for initialization
+	void Start () 
+    {
+        Play.onClick.AddListener(delegate { SceneManager.LoadScene("SaveMenu"); });
+        Options.onClick.AddListener(delegate { SceneManager.LoadScene("OptionsMenu"); });
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
