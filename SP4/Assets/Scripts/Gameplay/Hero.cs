daa0f35500458659d38a962c1f74d151ef5cd57f
guanhui1998 <lguanhui14@gmail.com>
2017-02-13 13:52:25 +0800

    pulling latest changes

---
+++ SP4/Assets/Scripts/Gameplay/Hero.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Hero : MonoBehaviour {
+    public static float Attack,Defense, Resistance, Speed, Accuracy, Evasion;
+    public static string name;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Chain attacks
+    void OneChain()
+    {
+
+    }
+
+    void TwoChain()
+    {
+
+    }
+
+    void ThreeChain()
+    {
+
+    }
+
+    // Normal attack
+    void NormalAttack()
+    {
+
+    }
+
+    // when attacked
+    void getHit()
+    {
+        //calculate how damage is taken here
+
+    }
+
+    // Special ability
+    public void SpecialAbility()
+    {
+
+    }
+
+
+}
