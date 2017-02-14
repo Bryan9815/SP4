1d05661da9341a8f2fcc22eaa3aa7cf9a7bf3b00
Bryan <btteh@hotmail.com>
2017-02-14 12:05:49 +0800

    Skeleton for game over screen complete.

---
+++ SP4/Assets/Scripts/GameOver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameOver : MonoBehaviour 
+{
+    public Image Hero1, Hero2, Hero3;
+    public Text Stats1, Stats2, Stats3, Wave, Gold;
+    public Button ReturnToMainMenu;
+    // Use this for initialization
+	void Start () 
+    {
+        // Use player team for hero images
+        //Stats1.text = "Kills: " + Hero1.kills.ToString();
+
+        //Wave.text = "Wave Reached: " + waveNo;
+        //Gold.text = "Gold Earned: " + goldEarned;
+
+        ReturnToMainMenu.onClick.AddListener(delegate { BackToMainMenu(); });
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void BackToMainMenu()
+    {
+        //if (waveNo > PlayerPrefs.GetInt("waveRecord", 0))
+        //    PlayerPrefs.SetInt("waveRecord", waveNo);
+        // Same for hero kills, etc
+        SceneManager.LoadScene("MainMenu");
+    }
+}
