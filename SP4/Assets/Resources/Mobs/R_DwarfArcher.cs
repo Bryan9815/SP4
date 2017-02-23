faa47218bdd5e7c1582d5f0702c53b41ad2791d1
Bryan <btteh@hotmail.com>
2017-02-23 15:39:49 +0800

    Changed animations for melee dwarves

--- SP4/Assets/Resources/Mobs/R_DwarfArcher.cs
+++ SP4/Assets/Resources/Mobs/R_DwarfArcher.cs
@@ -14,13 +14,13 @@ public class R_DwarfArcher : Mob
     States state;
     Animator animator;
     // Use this for initialization
-    protected override void Start()
+    void Start()
     {
         Hp = 10;
         Defense = 10;
 
-        attackTimer = 0.0f;
-        attackTimer_Max = 2.0f;
+        attackTimer = 2.0f;
+        attackTimer_Max = 3.0f;
 
         state = States.Idle;
         animator = GetComponent<Animator>();
@@ -29,34 +29,33 @@ public class R_DwarfArcher : Mob
     // Update is called once per frame
     protected override void Update()
     {
-        float distFromHero = Mathf.Sqrt((float)(gameObject.transform.position.x + Hero1.transform.position.x) * (gameObject.transform.position.x + Hero1.transform.position.x) + (gameObject.transform.position.y + Hero1.transform.position.y) * (gameObject.transform.position.y + Hero1.transform.position.y));
-        if (distFromHero <= 7.5)
+        float distFromHero = Mathf.Sqrt((float)(gameObject.transform.position.x + Hero1.transform.position.x) * (gameObject.transform.position.x + Hero1.transform.position.x));
+        if (distFromHero <= 7.5 && distFromHero > 5)
         {
             state = States.Walk;
             animator.SetTrigger("Target Detected");
+            animator.SetBool("No Targets", false);
+        }
+        else if (distFromHero > 7.5 && state != States.Attack)
+        {
+            state = States.Idle;
+            animator.SetBool("No Targets", true);
         }
         else if (distFromHero <= 5)
         {
             state = States.Attack;
             animator.SetBool("Targets In Range", true);
         }
-        else
-        {
-            state = States.Idle;
-            animator.SetTrigger("No Targets");
-        }
         switch (state)
         {
             case States.Idle:
-
                 foreach (GameObject hero in HeroList)
                 {
                     if (!gameObject.GetComponent<BoxCollider2D>().IsTouching(hero.GetComponent<HeroHolder>().Get_GameObject().GetComponent<BoxCollider2D>()))
                     {
                         Vector3 temp = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-                        temp.x -= Time.deltaTime * 2;
+                        temp.x -= Time.deltaTime;
                         gameObject.transform.position = temp;
-                        Debug.Log("DistFromHero: " + distFromHero);
                     }
                 }
                 break;
@@ -72,24 +71,28 @@ public class R_DwarfArcher : Mob
                 }
                 break;
             case States.Attack:
-                attackTimer += Time.deltaTime;
+                if (animator.GetFloat("Cooldown Timer") < attackTimer_Max)
+                    attackTimer += Time.deltaTime;
+                animator.SetFloat("Cooldown Timer", attackTimer);
+
+                Vector3 temp2 = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+
+                if (attackTimer >= attackTimer_Max)
+                {
+                    GameObject arrow = Instantiate(Arrow.GetComponent<ProjectileHolder>().Get_GameObject(), new Vector3(temp2.x - 1, temp2.y, temp2.z), Arrow.transform.rotation) as GameObject;
+                    arrow.SetActive(true);
+
+                    attackTimer = 0.0f;
+                    animator.SetFloat("Cooldown Timer", attackTimer);
+                    animator.SetTrigger("Cooldown");
+                }
+
                 foreach (GameObject hero in HeroList)
                 {
                     if (!gameObject.GetComponent<BoxCollider2D>().IsTouching(hero.GetComponent<HeroHolder>().Get_GameObject().GetComponent<BoxCollider2D>()))
                     {
-                        Vector3 temp = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-                        temp.x -= Time.deltaTime * 0.5f;
-                        gameObject.transform.position = temp;
-
-                        if (attackTimer >= attackTimer_Max)
-                        {
-                            GameObject arrow = Instantiate(Arrow, temp, Arrow.transform.rotation) as GameObject;
-                            attackTimer = 0.0f;
-                        }
-                    }
-                    else
-                    {
-                        // Shift to run away state to increase distance between heroes & archer?
+                        temp2.x -= Time.deltaTime * 0.5f;
+                        gameObject.transform.position = temp2;
                     }
                 }
                 break;
